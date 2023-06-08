6e59d688ea9703b22c6dbf5b3c5f259119de3b78
Thierry Stuhler <thierrystuhler@gmail.com>
2023-06-08 17:16:09 -0300

    Fazendo otimizações

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Data;
+﻿using System.Text.RegularExpressions;
+using Blog.Data;
 using Blog.Extensions;
 using Blog.Models;
 using Blog.Services;
@@ -8,141 +9,122 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
-using System.Drawing;
-using System.Text.RegularExpressions;
 
-namespace Blog.Controllers
+namespace Blog.Controllers;
+
+[ApiController]
+public class AccountController : ControllerBase
 {
-    [Authorize]
-    [ApiController]
-    public class AccountController : ControllerBase
+    [HttpPost("v1/accounts/")]
+    public async Task<IActionResult> Post(
+        [FromBody] RegisterViewModel model,
+        [FromServices] BlogDataContext context,
+        [FromServices] EmailService emailService)
     {
-        [HttpPost("v1/accounts/")]
-        public async Task<IActionResult> Post([FromBody] RegisterViewModel model, [FromServices] EmailService emailService, [FromServices] BlogDataContext context)
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+        var user = new User
         {
-            if (!ModelState.IsValid)
+            Name = model.Name,
+            Email = model.Email,
+            Slug = model.Email.Replace("@", "-").Replace(".", "-")
+        };
+
+        var password = PasswordGenerator.Generate(25);
+        user.PasswordHash = PasswordHasher.Hash(password);
+
+        try
+        {
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            emailService.Send(user.Name, user.Email, "Bem vindo ao blog!", $"Sua senha é {password}");
+            return Ok(new ResultViewModel<dynamic>(new
             {
-                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
-            }
+                user = user.Email,
+                password
+            }));
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(400, new ResultViewModel<string>("05X99 - Este E-mail já está cadastrado"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
+    }
 
-            var user = new User
-            {
-                Name = model.Name,
-                Email = model.Email,
-                Slug = model.Email.Replace("@", "-").Replace(".", "-")
-            };
+    [HttpPost("v1/accounts/login")]
+    public async Task<IActionResult> Login(
+        [FromBody] LoginViewModel model,
+        [FromServices] BlogDataContext context,
+        [FromServices] TokenService tokenService)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
 
-            return Ok();
+        var user = await context
+            .Users
+            .AsNoTracking()
+            .Include(x => x.Roles)
+            .FirstOrDefaultAsync(x => x.Email == model.Email);
 
-            var password = PasswordGenerator.Generate(25);
-            user.PasswordHash = PasswordHasher.Hash(password);
+        if (user == null)
+            return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
 
-            try
-            {
-                await context.Users.AddAsync(user);
-                await context.SaveChangesAsync();
+        if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+            return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
 
-                emailService.Send(
-                    user.Name,
-                    user.Email,
-                    subject: "Bem vindo ao blog!",
-                    body: $"Sua senha é <strong>{password}<strong>");
+        try
+        {
+            var token = tokenService.GenerateToken(user);
+            return Ok(new ResultViewModel<string>(token, null));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
+    }
 
-                return Ok(new ResultViewModel<dynamic>(new
-                {
-                    user = user.Email,
-                    password
-                }));
-            }
-            catch (DbUpdateException)
-            {
-                return StatusCode(400, new ResultViewModel<string>("05X99 - Este e-mail já está cadastrado."));
-            }
-            catch
-            {
-                return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna."));
-            }
+    [Authorize]
+    [HttpPost("v1/accounts/upload-image")]
+    public async Task<IActionResult> UploadImage(
+        [FromBody] UploadImageViewModel model,
+        [FromServices] BlogDataContext context)
+    {
+        var fileName = $"{Guid.NewGuid().ToString()}.jpg";
+        var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(model.Base64Image, "");
+        var bytes = Convert.FromBase64String(data);
+
+        try
+        {
+            await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
         }
 
-        [HttpPost("v1/accounts/login")]
-        public async Task<IActionResult> Login(
-            [FromBody] LoginViewModel model,
-            [FromServices] BlogDataContext context,
-            [FromServices] TokenService tokenService)
+        var user = await context
+            .Users
+            .FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+
+        if (user == null)
+            return NotFound(new ResultViewModel<Category>("Usuário não encontrado"));
+
+        user.Image = $"https://localhost:0000/images/{fileName}";
+        try
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
-            }
-
-            var user = await context
-                .Users
-                .AsNoTracking()
-                .Include(x => x.Roles)
-                .FirstOrDefaultAsync(x => x.Email == model.Email);
-
-            if (user == null)
-            {
-                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválido."));
-            }
-
-            if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
-            {
-                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválido."));
-            }
-
-            try
-            {
-                var token = tokenService.GenerateToken(user);
-                return Ok(new ResultViewModel<string>(token, null));
-            }
-            catch
-            {
-                return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna."));
-            }
+            context.Users.Update(user);
+            await context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
         }
 
-        [Authorize]
-        [HttpPost("v1/accounts/upload-image")]
-        public async Task<IActionResult> UploadImage(
-            [FromBody] UploadImageViewModel model,
-            [FromServices] BlogDataContext context)
-        {
-            var fileName = $"{Guid.NewGuid().ToString()}.jpg";
-            var data = new Regex(@"^data:image V [a-z]+;base64,")
-                .Replace(model.Base64Image, "");
-            var bytes = Convert.FromBase64String(data);
-
-            try
-            {
-                await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna."));
-            }
-
-            var user = await context
-                .Users
-                .FirstOrDefaultAsync(X => X.Email == User.Identity.Name);
-
-            if(user == null)
-            {
-                return NotFound(new ResultViewModel<Category>("Usuário não enconttrado."));
-            }
-
-            user.Image = $"https://localhost:0000/images/{fileName}";
-
-            try
-            {
-                context.Users.Update(user);
-                await context.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResultViewModel<string>("05X05 - Falha interna."));
-            }
-            return Ok();
-        }
+        return Ok(new ResultViewModel<string>("Imagem alterada com sucesso!", null));
     }
 }
--- ViewModels/Accounts/UploadImageViewModel.cs
+++ ViewModels/Accounts/UploadImageViewModel.cs
@@ -1,10 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Blog.ViewModels.Accounts
-{
-    public class UploadImageViewModel
-    {
-        [Required(ErrorMessage = "Imagem inválida!")]
+namespace Blog.ViewModels.Accounts;
 
-    }
+public class UploadImageViewModel
+{
+    [Required(ErrorMessage = "Imagem inválida")]
+    public string Base64Image { get; set; }
 }
